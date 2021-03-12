4c861bf1ca3d53860da1f8334e35a13f861f9768
akshat-accolite <akshat.mehta@accolitedigital.com>
2021-03-12 23:19:51 +0530

    screenshots with explaination

--- opportunityManagement/opportunitymanagement/src/main/java/com/accolite/opportunitymanagement/controller/OpportunityController.java
+++ opportunityManagement/opportunitymanagement/src/main/java/com/accolite/opportunitymanagement/controller/OpportunityController.java
@@ -1,14 +1,20 @@
 package com.accolite.opportunitymanagement.controller;
 
-import com.accolite.opportunitymanagement.mapper.OpportunityMapper;
-import com.accolite.opportunitymanagement.model.Opportunity;
-import com.accolite.opportunitymanagement.service.Impl.OpportunityServiceImpl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accolite.opportunitymanagement.model.Opportunity;
+import com.accolite.opportunitymanagement.service.Impl.OpportunityServiceImpl;
 
 @RestController
 @RequestMapping("/opportunity")
@@ -45,6 +51,4 @@ public class OpportunityController {
     {
         return opportunityServiceImpl.delete(id);
     }
-
-
 }
--- opportunityManagement/opportunitymanagement/src/test/java/com/accolite/opportunitymanagement/service/TrendServiceImplTest.java
+++ opportunityManagement/opportunitymanagement/src/test/java/com/accolite/opportunitymanagement/service/TrendServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.accolite.opportunitymanagement.service;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,26 +7,23 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.web.servlet.MockMvc;
 
-import com.accolite.opportunitymanagement.controller.TrendController;
-import com.accolite.opportunitymanagement.model.Opportunity;
 import com.accolite.opportunitymanagement.service.Impl.TrendServiceImpl;
 
-//@WebMvcTest(service = TrendController.class)
+//@WebMvcTest(controllers = TrendController.class)
 @SpringBootTest
 public class TrendServiceImplTest {
 	
-	@Autowired
-    public MockMvc mockMvc;
+//	@Autowired
+//    public MockMvc mockMvc;
 
     @InjectMocks
     public TrendServiceImpl trendServiceImpl;
@@ -40,14 +36,15 @@ public class TrendServiceImplTest {
     
 //    @Test
 //    public void getLocationTest() {
-//    	jdbcTemplate=new JdbcTemplate(new DataSource());
+//    	jdbcTemplate=new JdbcTemplate(datasource);
 //    	List<String> LocationList = new ArrayList<String>();
 //    	LocationList.add("Mumbai");
-//    	List<Map<String,Object>> mockRows=new ArrayList<>();
-//		HashMap<String,Object> param=new HashMap<String,Object>();
-//		param.put("location",new Opportunity(1,"Java Developer","Mumbai","SpringBoot","accolite.digital@accolite.digital.com",3,5,new Date(System.currentTimeMillis())));
+//    	List<Map<String,Object>> mockRows=new ArrayList<Map<String, Object>>();
+//		Map<String,Object> param=new HashMap<String,Object>();
+//		param.put("location","Mumbai");
 //		mockRows.add(param);
-//    	Mockito.when(jdbcTemplate.queryForList(Mockito.anyString())).thenReturn(mockRows);
-//    	trendServiceImpl.getByLocation();
+//    	Mockito.when(jdbcTemplate.queryForList("select distinct location from Opportunity")).thenReturn(mockRows);
+//    	List<String> resList=trendServiceImpl.getByLocation();
+//    	Assert.assertEquals(LocationList.size(), resList.size());
 //    }
 }
